c988702663d9c515c7d3c1b9b7ace2d5366b79a6
longd+huyd <long.dang+huy.dinh@eastagile.com>
2014-05-29 10:30:38 +0700

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicRank::Application.config.secret_key_base = 'f4de34e954c018d1eb2142cf2d29e5b2735979c4dc17778a511b8410d9fa88fc484495fecfee468d8d18d1d1be7ec1957161ca88f604013e73b377a6f25a5bf1'
